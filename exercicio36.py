055e91840e1dd3bc820bec5c4db586fc25d1c40c
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-07-24 15:37:57 -0300

    Aprovando Empréstimo
    Exercício Python #036 - Aprovando Empréstimo CURSO EM VIDEO

--- exercicio36.py
+++ exercicio36.py
@@ -3,6 +3,7 @@ salario = float(input('Qual é o seu salario mensal? R$'))
 anos = int(input('Em quantos anos vc quer pagar? '))
 prestacao = casa / (anos * 12)
 minimo = salario * 30 / 100
+# A virgula representa o milhar
 print('Para pagar uma casa de R${:,.2f} em {} anos '.format(casa, anos), end='') 
 print(' a prestação fica em R${:,.2f}'.format(prestacao))
 if prestacao <= minimo:
