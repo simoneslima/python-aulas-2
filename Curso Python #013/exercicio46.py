f000a1cf1a72fdad6d4e22f74178fe70168456e1
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-08-15 15:39:22 -0300

    Contagem regressiva
    Exercício Python #046 - Contagem regressiva

---
+++ Curso Python #013/exercicio46.py
@@ -0,0 +1,6 @@
+print('\033[7;0;32mContagem Regressiva!!!\033[m')
+from time import sleep
+for contagem in range(10, 0, -1):
+    sleep(1)
+    print(contagem)
+print('\033[7;0;32mFeliz Ano Novo!!!\033[m')
