e8d7470c195b5d09c4636fb4ed238cd82f556d9e
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-08-22 15:54:44 -0300

    Soma ímpares múltiplos de três
    Corrigido Soma ímpares múltiplos de três

--- Curso Python #013/exercicio48.py
+++ Curso Python #013/exercicio48.py
@@ -1,5 +1,15 @@
-total = sum(impar for impar in range(1, 500, 3))
-print('A soma de todos os números no intervalo de 3 é {}'.format(total))
+soma = 0 #acumulador
+cont = 0 #contador
+for contador in range(1, 501, 2):
+    if contador % 3 == 0:
+        cont = cont + 1
+        soma = soma + contador
+print('A soma de todos os {} números no intervalo de 3 é {}'.format(cont,soma))
+    
+    
+        
+    
+   
 
     
     
