52d626434cdbb5e55fdad44ef877ee35e7f4fbfd
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-08-28 16:52:04 -0300

    Soma dos pares (Correção)
    Exercício Python #050 - Soma dos pares

--- Curso Python #013/exercicio50.py
+++ Curso Python #013/exercicio50.py
@@ -1,17 +1,9 @@
-n1 = int(input('Digite O Primeiro numero para soma:'))
-n2 = int(input('Digite O Segundo numero para soma:'))
-n3 = int(input('Digite O Terceiro numero para soma:'))
-n4 = int(input('Digite O Quarto numero para soma:'))
-n5 = int(input('Digite O Quinto numero para soma:'))
-n6 = int(input('Digite O Sexto e ultimo numero para soma:'))
-
-# Coloca os números em uma lista
-lista = [n1, n2, n3, n4, n5, n6]
-
 soma = 0
-
-for numero in lista:
-    if numero % 2 == 0:
-        soma += numero  # Soma apenas os números pares
-
-print('A soma dos números pares é: {}'.format(soma))
+cont = 0
+for contador in range(1, 7):
+  numero = int(input('Digite o {}° numero para soma:'.format(contador)))
+  if numero % 2 == 0:
+    soma = soma + numero
+    cont = cont + 1
+print('Foi informado {} numeros pares e a soma deles são {}'.format(cont, soma))
+  
