93bfea42b1ae616bfd8fed045f2cb6a8691c7106
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-09-04 16:14:38 -0300

    Números primos
    Exercício Python #052 - Números primos Corrigido.

--- Curso Python #013/exercicio52.py
+++ Curso Python #013/exercicio52.py
@@ -1,10 +1,17 @@
-from sympy import isprime 
-
-# Solicita ao usuário um número para verificar se é primo
-numero = int(input("Digite um número: "))
-
-if isprime(numero):
-    print('{} É um número primo.'.format(numero))
+num = int(input('Digite um Número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{}'.format(c), end=' ')
+    
+print('\n\033[mO número {} foi divisível {} vezes'.format(num, tot))
+if tot == 2:
+    print('E por isso é um número primo!')
 else:
-    print('Não é um número primo.'.format(numero))
+    print('E por isso não é um número primo!')
+
 
