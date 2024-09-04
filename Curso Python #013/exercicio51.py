3a8519f7b07d45cbb4fe85d3fa6c0f30cf1ad6c6
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-09-04 15:51:19 -0300

    calcular PA
    corrigido

--- Curso Python #013/exercicio51.py
+++ Curso Python #013/exercicio51.py
@@ -3,7 +3,8 @@ primeiro_termo = int(input("Digite o primeiro termo da PA: "))
 # diferença entre os termos
 razao = int(input("Digite a razão da PA: "))
 
-print("Os 10 primeiros termos da PA são:" )
+print("Os 10 primeiros termos da PA são:", )
 for i in range(10):
     termo = primeiro_termo + i * razao
-    print(termo)
+    print(termo, end= '->')
+print('FIM')    
