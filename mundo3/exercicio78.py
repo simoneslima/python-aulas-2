5f3f65b85706218ca5fcf9e4b71ce686ffbca20e
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-12-10 15:18:03 -0300

    Maior e Menor valores na Lista
    Exercício Python #078 - Maior e Menor valores na Lista (Corrigido).

--- mundo3/exercicio78.py
+++ mundo3/exercicio78.py
@@ -1,13 +1,25 @@
 numeros = []
+maximo = 0  
+minimo = 0
 for num in range(0, 5):
-    numeros.append(int(input('Digite um Valor: ')))
-
-maximo = max(numeros)    
-minimo = min(numeros)
+    numeros.append(int(input(f'Digite um Valor na posição {num}: ')))
+    if num == 0:
+        maximo = minimo = numeros[num]
+    else:
+        if numeros[num] > maximo:
+            maximo = numeros[num]
+        if numeros[num] < minimo:
+            minimo = numeros[num]
+print('=-='* 30)
 print(f'Voce digitou os numeros {numeros}.')
-
-posicoes_max = [i+1 for i, x in enumerate(numeros) if x == maximo]
-posicoes_min = [i+1 for i, x in enumerate(numeros) if x == minimo]
-print(f'O maior valor foi o numero {maximo} que ficou na posições {posicoes_max}.')
-print(f'O menor valor foi o numero {minimo} que ficou na posições {posicoes_min}.')
+print(f'O maior valor foi o numero {maximo} que ficou nas posições.', end='')
+for i, v in enumerate(numeros):
+    if v == maximo:
+        print(f'{i}, ', end='')
+print()
+print(f'O menor valor foi o numero {minimo} que ficou nas posições.', end='')
+for i, v in enumerate(numeros):
+    if v == minimo:
+        print(f'{i}, ', end='')
+print()
 
