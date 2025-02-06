e7da67b0de1611b8d86e12d88f31b87fa6def496
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2025-02-06 17:20:24 -0300

    Desafio 90
    feito

---
+++ aula19/desafio90.py
@@ -0,0 +1,11 @@
+nome = {}
+media = {}
+nome = str(input('Nome:'))
+media = float(input('Media: '))
+print(f'O nome é {nome}.')
+print(f'A Media é {media}.')
+if media >= 5:
+    print('Vc esta aprovada! Parabens')
+else: 
+    print('Reprovado!')
+
